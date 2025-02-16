d308fec3b8bceaf2f06d1e97e2f3e43f56851560
Wiliam <WilliamAlbano28@gmail.com>
2025-02-16 18:09:27 +0000

    commit

--- backEnd/Dbconnection.js
+++ backEnd/Dbconnection.js
@@ -3,14 +3,18 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD || '',  // Usando uma senha em branco, se não definida
-  database: process.env.DB_NAME
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306,
+  connectTimeout: 10000 // Aumenta o tempo de conexão
 });
 
 connection.connect((err) => {
   if (err) {
-    console.error('Database connection error: ', err.stack);
+    console.error("Erro na conexão com o banco de dados:", err);
     return;
   }
-  console.log('Connected to the database!');
+  console.log("Conectado ao banco de dados!");
 });
+
+module.exports = connection;
